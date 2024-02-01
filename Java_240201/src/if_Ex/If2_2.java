6884346b4f5efb3c72851cc1549808cf724190d5
rang <range940829@gmail.com>
2024-02-02 01:35:43 +0900

    Java 240201

--- Java_240201/src/if_Ex/If2_2.java
+++ Java_240201/src/if_Ex/If2_2.java
@@ -1,32 +1,20 @@
 package if_Ex;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class If2_2 {
 
-	public static void main(String[] args) {
-		int age = 60;
-		int charge;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		if (age < 8) {
-			charge = 1000;
-			System.out.println("취학 전 아동입니다.");
-		}
-		else if(age < 14) {
-			charge = 2000;
-			System.out.println("초등학생입니다.");
-		}
-		else if(age < 20) {
-			charge = 2500;
-			System.out.println("중, 고등학생입니다.");
-		}		
-		else if(age < 60) {
-			charge = 3000;
-			System.out.println("일반인입니다.");
-		}
-		else {
-			charge = 0;
-			System.out.println("경로우대입니다.");
-		}
+		BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("점수를 입력하세요 : ");
+		int score = Integer.parseInt(inbr.readLine());
+		char grade;
 		
-			System.out.println("입장료는 " + charge + "원입니다.");
-		}
+		grade = (score >= 90 ? 'A' : (score >= 80 ? 'B': (score >= 70 ? 'C' : (score >= 60 ? 'D' :'F'))));
+
+		System.out.println("성적은 " + score + "이고, 학점은 " + grade + "입니다.");
 	}
+}
