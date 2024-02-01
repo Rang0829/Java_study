95a62cbdf1d6b161de51dcc463c16a4c5f550407
rang <range940829@gmail.com>
2024-02-01 23:44:17 +0900

    Java 240201

--- Java2401/src/operator/Operation4_2.java
+++ Java2401/src/operator/Operation4_2.java
@@ -8,7 +8,7 @@ public class Operation4_2 {
 		boolean isEven;
 		isEven = (num % 2 == 0) ? true : false;
 // num을 2로 나누었을 때 나머지 값이 0이 나오면 짝수이기 때문에 나머지 연산자를 사용한다.
-// 나머지 값이 0이면 참이라서 true 출력, 아니라면 거짓이라서 false 출력된다.	
+// 나머지 값이 0이면 참이라서 true 출력, 아니라면 거짓이라서 false 출력된다.
 		System.out.println(isEven);
 	}
 }
