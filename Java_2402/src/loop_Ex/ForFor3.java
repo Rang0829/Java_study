518394703cf4c3fb9d65a6970acdb87bf2afbe95
Rang0829 <range940829@gmail.com>
2024-02-17 18:37:54 +0900

    Java 240217

--- Java_2402/src/loop_Ex/ForFor3.java
+++ Java_2402/src/loop_Ex/ForFor3.java
@@ -2,13 +2,13 @@ package loop_Ex;
 
 public class ForFor3 {
 
-	  public static void main(String[] args){
+	public static void main(String[] args) {
 
-	    for (int i = 1; i <= 5; i++) {
-	      for (int j = 1; j <= i; j++) {
-	        System.out.print("*");
-	      }
-	      System.out.println(); // 줄바꿈
-	      }
-	  }
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(); // 줄바꿈
+		}
 	}
+}
