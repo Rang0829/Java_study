126cbdb78211c9e5038ed1e20ab037863937db74
Rang0829 <range940829@gmail.com>
2024-02-29 20:42:42 +0900

    Java 240229

--- Java_2402/src/oop_Ex/TimeTest.java
+++ Java_2402/src/oop_Ex/TimeTest.java
@@ -6,9 +6,14 @@ class Time {
 	private int second;
 	
 	public void setHour(int hour) {
-		if(hour < 0 || hour > 23) return;
+		if(isNotValidHour(hour)) return;
 		
 		this.hour = hour;
+	}
+
+	// 매개변수로 넘겨진 hour가 유효한지 확인해서 알려주는 메서드.
+	private boolean isNotValidHour(int hour) {
+		return hour < 0 || hour > 23;
 	}
 	
 	public int getHour() {
@@ -24,6 +29,5 @@ public class TimeTest {
 		System.out.println(t.getHour());
 		t.setHour(100);
 		System.out.println(t.getHour());
-
 	}
 }
