b52733ab0b53a34b9e64c3ce6b39bb6a2bbf75f7
Rang0829 <range940829@gmail.com>
2024-04-22 23:54:17 +0900

    Java 240422

--- Java_2404/src/Object_Ex/Object1.java
+++ Java_2404/src/Object_Ex/Object1.java
@@ -19,11 +19,14 @@ class Value {
         this.value = value;
     }
     
-    public boolean equals(Object obj) {      
-        if (!(obj instanceof Value)) // obj가 Value 클래스의 인스턴스인지 확인
+    // Object의 equals()를 오버라이딩해서 주소가 아닌 value 값을 비교.
+    public boolean equals(Object obj) {
+//    	return this == obj;   // 주소 비교. 서로 다른 객체는 항상 거짓.
+    	// 참조변수의 형변환 전에는 반드시 instanceof로 확인.
+        if (!(obj instanceof Value))   // obj가 Value 클래스의 인스턴스인지 확인
             return false;
         
-        Value v = (Value) obj; // obj를 Value로 형 변환
+        Value v = (Value) obj; // obj를 Value로 형 변환.
         return this.value == v.value;
 	}
 }
